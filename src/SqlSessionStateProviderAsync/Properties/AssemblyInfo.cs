2eea7f712f9ad6863d2f077c551073bb928637f9
jinhuafei <mattfei@microsoft.com>
2016-09-01 09:25:12 -0700

    update codesign task

--- src/SqlSessionStateProviderAsync/Properties/AssemblyInfo.cs
+++ src/SqlSessionStateProviderAsync/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Microsoft.AspNet.SessionState.SqlSessionStateProviderAsync")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("\x00a9 Microsoft Corporation. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
